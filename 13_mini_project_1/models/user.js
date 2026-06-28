5d65daa956a1fb1b278584a7a85a5ec2be4b9a55
Ritesh Patel <riteshpatel1882004@gmail.com>
2026-06-28 09:03:11 +0530

    multer added

---
+++ 13_mini_project_1/models/user.js
@@ -0,0 +1,22 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://ritesh20047_db_user:mongodbpassword@cluster0.yrge0ja.mongodb.net/?appName=Cluster0")
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.log(err));
+
+const userSchema = mongoose.Schema({
+    username:String,
+    name:String,
+    password:String,
+    age:Number,
+    email:String,
+    posts:[
+        {type:mongoose.Schema.Types.ObjectId, ref:"post"}
+    ]
+})
+
+
+
+module.exports = mongoose.model("user", userSchema);
